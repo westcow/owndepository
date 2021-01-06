b9bae4cd5c814b7ecb2a5eadc0b0711d59c46534
westcowboy <1024668091@qq.com>
2021-01-06 15:44:38 +0800

    实体类添加lombok 有参构造和无参构造注解

--- src/main/java/com/bjb/project/entity/Database.java
+++ src/main/java/com/bjb/project/entity/Database.java
@@ -1,8 +1,12 @@
 package com.bjb.project.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Database {
     private Integer sort;
     private String IP;
