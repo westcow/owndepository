540b191974b393c8c8c8b47bb6253b5bf6cf6597
westcowboy <1024668091@qq.com>
2021-01-06 15:50:25 +0800

    实体类补充新字段

--- src/main/java/com/bjb/project/entity/Database.java
+++ src/main/java/com/bjb/project/entity/Database.java
@@ -1,8 +1,12 @@
 package com.bjb.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,4 +19,11 @@ public class Database {
     private String title;
     private String userName;
     private String password;
+    private Integer delFlg;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createDatetime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateDatetime;
 }
--- src/main/java/com/bjb/project/entity/QueryExport.java
+++ src/main/java/com/bjb/project/entity/QueryExport.java
@@ -1,8 +1,12 @@
 package com.bjb.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +17,11 @@ public class QueryExport {
     private String sqlMessage;
     private String dbName;
     private String templateUrl;
+    private Integer delFlg;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createDatetime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateDatetime;
 }
